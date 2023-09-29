5c52eae94949fc8e97c2f0f00e921105d8340623
Matias Duran <matiduraan@gmail.com>
2023-09-28 22:32:26 -0300

    Add community mock

---
+++ mocks/mockedCommunity.js
@@ -0,0 +1,11 @@
+return {
+  id: 1,
+  miembros: [
+    { id_usuario: "user1234", grado_confianza: 1.5 },
+    { id_usuario: "user5678", grado_confianza: 2.0 },
+    { id_usuario: "user9012", grado_confianza: 3.0 },
+    { id_usuario: "user3456", grado_confianza: 3.5 },
+    { id_usuario: "user7890", grado_confianza: 4.0 },
+    { id_usuario: "userabcd", grado_confianza: 4.5 },
+  ],
+};
